69a5e146a0be2c232d82c441a6e87f492fb9a26a
SodAcetate <89978222+SodAcetate@users.noreply.github.com>
2023-02-02 17:25:14 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,7 +3,7 @@ include <string>
 using namespace std;
 int main() {
 string name;
-// Бабушка технарь, дед гуманитарий
+// Я изобрёл универсальный комментарий
 cout << "Please enter name";
 cin >> name;
 cout << "Hello world from " << name;
