73d542b3f2f7df574f2b91f977965b5d5bb95ce9
SodAcetate <sodium.acetate.82@gmail.com>
2023-02-02 16:05:12 +0300

    added comment

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,6 +3,7 @@ include <string>
 using namespace std;
 int main() {
 string name;
+// Бабушка технарь, дед гуманитарий
 cout << "Please enter name";
 cin >> name;
 cout << "Hello world from " << name;
