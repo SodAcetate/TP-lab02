ee780f2600b039caa7ac2639b819beea744302ab
SodAcetate <sodium.acetate.82@gmail.com>
2023-02-02 17:59:10 +0300

    changed hellodsffsdf

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
 string name;
 // Я изобрёл универсальный комментарий
-cout << "Please enter name";
+cout << "Please enter your name";
 cin >> name;
 cout << "Hello world from " << name;
 }
